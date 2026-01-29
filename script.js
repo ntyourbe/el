261f656cacb62b2f577cbd73c9a4057403c842ea
ntyourbe <cs@pradani.eu.org>
2026-01-30 02:27:27 +0700

    Update script.js

--- script.js
+++ script.js
@@ -587,10 +587,10 @@ scene.add(planet);
 
 //Ganti Deskripsi Planet
 const ringTexts = [
-  'Galaxy of love From ...',//untuk deskripsi planet layer 1
-  "I love you",//untuk deskripsi planet layer 2
-  "♡Happy Girlfriend day♡",//untuk deskripsi planet layer 3
-  "01/08/2025",//untuk deskripsi planet layer 4
+  'Galaxy love From Dani',//untuk deskripsi planet layer 1
+  "I love you Bbyy",//untuk deskripsi planet layer 2
+  "Maafin aku yaa",//untuk deskripsi planet layer 3
+  "30/01/2026",//untuk deskripsi planet layer 4
   ...(window.dataCCD && window.dataCCD.data.ringTexts ? window.dataCCD.data.ringTexts : [])
 ];
 
